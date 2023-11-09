243371f83590ea9c418dfd56c714f5e7afe23e98
@plotvichka_2 <leshaharhalev@gmail.com>
2023-11-09 09:59:31 +0300

    Create TestTask

---
+++ lab-01/src/by/Lexus_FAMCS/quizer/tasks/TextTask.java
@@ -0,0 +1,34 @@
+package by.Lexus_FAMCS.quizer.tasks;
+
+import by.Lexus_FAMCS.quizer.Result;
+import by.Lexus_FAMCS.quizer.task_generators.PoolTaskGenerator;
+
+/**
+ * Задание с заранее заготовленным текстом.
+ * Можно использовать {@link PoolTaskGenerator}, чтобы задавать задания такого типа.
+ */
+class TextTask implements Task {
+    /**
+     * @param text   текст задания
+     * @param answer ответ на задание
+     */
+    String text;
+    String answer;
+    TextTask(
+            String text,
+            String answer
+    ) {
+        this.text = text;
+        this.answer = answer;
+    }
+
+    @Override
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public Result validate(String answer) {
+        return text.equals(answer) ? Result.OK : Result.WRONG;
+    }
+}
