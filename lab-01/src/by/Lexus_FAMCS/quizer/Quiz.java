b815efcb980949acc5cddcb0640f78b8b8d7a8d8
@plotvichka_2 <leshaharhalev@gmail.com>
2023-11-09 17:53:35 +0300

    Implement Quiz

--- lab-01/src/by/Lexus_FAMCS/quizer/Quiz.java
+++ lab-01/src/by/Lexus_FAMCS/quizer/Quiz.java
@@ -7,12 +7,20 @@ import by.Lexus_FAMCS.quizer.tasks.Task;
  * Class, который описывает один тест
  */
 class Quiz {
+    private TaskGenerator gen;
+    private int taskCount;
+    private int taskNumber = -1;
+    private Task currTask;
+    private int correctAnswers;
+    private int incorrectInputAnswers;
+    private boolean skip = false;
     /**
      * @param generator генератор заданий
      * @param taskCount количество заданий в тесте
      */
     Quiz(TaskGenerator generator, int taskCount) {
-        // ...
+        gen = generator;
+        this.taskCount = taskCount;
     }
 
     /**
@@ -20,7 +28,17 @@ class Quiz {
      * @see Task
      */
     Task nextTask() {
-        // ...
+        if (isFinished()) {
+            return null;
+        } else {
+            if (skip) {
+                skip = false;
+                return currTask;
+            } else {
+                ++taskNumber;
+                return gen.generate();
+            }
+        }
     }
 
     /**
@@ -28,35 +46,40 @@ class Quiz {
      * ответов не увеличивается, а {@link #nextTask()} в следующий раз вернет тот же самый объект {@link Task}.
      */
     Result provideAnswer(String answer) {
-        // ...
+        Result res = currTask.validate(answer);
+        switch (res) {
+            case Result.OK -> ++correctAnswers;
+            case Result.INCORRECT_INPUT -> { ++incorrectInputAnswers; skip = true; }
+        }
+        return res;
     }
 
     /**
      * @return завершен ли тест
      */
     boolean isFinished() {
-        // ...
+        return taskNumber == taskCount;
     }
 
     /**
      * @return количество правильных ответов
      */
     int getCorrectAnswerNumber() {
-        // ...
+        return correctAnswers;
     }
 
     /**
      * @return количество неправильных ответов
      */
     int getWrongAnswerNumber() {
-        // ...
+        return taskCount - correctAnswers;
     }
 
     /**
      * @return количество раз, когда был предоставлен неправильный ввод
      */
     int getIncorrectInputNumber() {
-        // ...
+        return incorrectInputAnswers;
     }
 
     /**
@@ -64,6 +87,6 @@ class Quiz {
      *         Оценка выставляется только в конце!
      */
     double getMark() {
-        // ...
+        return (double) correctAnswers / taskCount;
     }
 }
