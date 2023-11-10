c7c68bb02ef62f2408b71e2c4cd6aff65b9717f6
@plotvichka_2 <leshaharhalev@gmail.com>
2023-11-10 07:03:53 +0300

    Create exception classes

---
+++ lab-01/src/by/Lexus_FAMCS/quizer/exceptions/IncorrectTestCreated.java
@@ -0,0 +1,6 @@
+package by.Lexus_FAMCS.quizer.exceptions;
+
+public class IncorrectTestCreated extends RuntimeException {
+    public IncorrectTestCreated() {}
+    public IncorrectTestCreated(String message) { super(message); }
+}
---
+++ lab-01/src/by/Lexus_FAMCS/quizer/exceptions/QuizNotFinishedException.java
@@ -0,0 +1,5 @@
+package by.Lexus_FAMCS.quizer.exceptions;
+public class QuizNotFinishedException extends RuntimeException{
+    public QuizNotFinishedException() {}
+    public QuizNotFinishedException(String message) { super(message); }
+}
