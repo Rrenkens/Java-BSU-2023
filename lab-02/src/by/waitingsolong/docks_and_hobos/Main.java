810fc72c018be23bc024d3379f0e3c9c51be7134
arseniy <snow_2015@list.ru>
2023-11-23 00:56:55 +0300

    logs

---
+++ lab-02/src/by/waitingsolong/docks_and_hobos/Main.java
@@ -0,0 +1,15 @@
+package by.waitingsolong.docks_and_hobos;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Start of program.");
+
+        logger.info("End of program.");
+    }
+
+}
