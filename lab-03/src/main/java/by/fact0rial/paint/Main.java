89868e1082b582d91380d42ba8a7ec1f6418bf96
Pasha Bernyak <pasha.bernyak@gmail.com>
2023-12-24 23:37:49 +0300

    add paint

---
+++ lab-03/src/main/java/by/fact0rial/paint/Main.java
@@ -0,0 +1,24 @@
+package by.fact0rial.paint;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1800, 1000);
+        stage.setTitle("Paint");
+        stage.setMaximized(true);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
