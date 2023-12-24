b1efeb0aa14317236d8477268b200544fce93c10
Busskov <bus9ko@gmail.com>
2023-12-24 15:23:06 +0300

    Add flood fill

--- lab-03/Paint/src/main/kotlin/by/busskov/paint/Main.kt
+++ lab-03/Paint/src/main/kotlin/by/busskov/paint/Main.kt
@@ -16,10 +16,15 @@ import javafx.scene.paint.Color
 import javafx.stage.Stage
 import javafx.scene.image.Image
 import javafx.scene.image.ImageView
+import javafx.scene.image.PixelReader
+import javafx.scene.image.PixelWriter
 import javafx.scene.layout.GridPane
+import javafx.scene.shape.Line
 import javafx.scene.shape.StrokeLineCap
+import java.util.*
 import kotlin.math.min
 import kotlin.math.abs
+import kotlin.math.roundToInt
 
 class PaintApp : Application() {
     private val canvas1: Canvas = Canvas(800.0, 600.0)
@@ -29,11 +34,17 @@ class PaintApp : Application() {
     private val toolBar: ToolBar = ToolBar()
     private val menuBar: MenuBar = MenuBar()
     private var paintType: PaintType = PaintType.CURVED_LINE
+    private var fillColor: Color = Color.BLACK
 
     override fun start(primaryStage: Stage) {
         configureMenuBar()
         configureToolBar()
+        graphicsContext1.fill = Color.WHITE
+        graphicsContext1.fillRect(0.0, 0.0, canvas1.width, canvas1.height)
+        graphicsContext1.fill = Color.BLACK
         graphicsContext1.lineCap = StrokeLineCap.ROUND
+        graphicsContext1.lineWidth = 2.0
+        graphicsContext2.lineWidth = 2.0
 
         val gridPane = GridPane()
         gridPane.add(menuBar, 0, 0)
@@ -49,32 +60,38 @@ class PaintApp : Application() {
         var baseX = 0.0
         var baseY = 0.0
         canvas2.setOnMousePressed { event ->
-            baseX = event.x
-            baseY = event.y
+            baseX = round(event.x)
+            baseY = round(event.y)
+        }
+
+        canvas2.setOnMouseClicked { event ->
+            if (paintType == PaintType.FILLING) {
+                floodFill(event.x.toInt(), event.y.toInt())
+            }
         }
 
         canvas2.setOnMouseDragged { event ->
             when(paintType) {
                 PaintType.CURVED_LINE -> {
-                    graphicsContext1.strokeLine(baseX, baseY, event.x, event.y)
-                    baseX = event.x
-                    baseY = event.y
+                    graphicsContext1.strokeLine(baseX, baseY, round(event.x), round(event.y))
+                    baseX = round(event.x)
+                    baseY = round(event.y)
                 }
                 PaintType.OVAL -> {
                     graphicsContext2.clearRect(0.0, 0.0, canvas2.width, canvas2.height)
                     graphicsContext2.strokeOval(
-                        min(baseX, event.x),
-                        min(baseY, event.y),
-                        abs(event.x - baseX),
-                        abs(event.y - baseY))
+                        round(min(baseX, event.x)),
+                        round(min(baseY, event.y)),
+                        round(abs(event.x - baseX)),
+                        round(abs(event.y - baseY)))
                 }
                 PaintType.RECTANGLE -> {
                     graphicsContext2.clearRect(0.0, 0.0, canvas2.width, canvas2.height)
                     graphicsContext2.strokeRect(
-                        min(baseX, event.x),
-                        min(baseY, event.y),
-                        abs(event.x - baseX),
-                        abs(event.y - baseY))
+                        round(min(baseX, event.x)),
+                        round(min(baseY, event.y)),
+                        round(abs(event.x - baseX)),
+                        round(abs(event.y - baseY)))
                 }
                 else -> {}
             }
@@ -85,18 +102,18 @@ class PaintApp : Application() {
                 PaintType.OVAL -> {
                     graphicsContext2.clearRect(0.0, 0.0, canvas2.width, canvas2.height)
                     graphicsContext1.strokeOval(
-                        min(baseX, event.x),
-                        min(baseY, event.y),
-                        abs(event.x - baseX),
-                        abs(event.y - baseY))
+                        round(min(baseX, event.x)),
+                        round(min(baseY, event.y)),
+                        round(abs(event.x - baseX)),
+                        round(abs(event.y - baseY)))
                 }
                 PaintType.RECTANGLE -> {
                     graphicsContext2.clearRect(0.0, 0.0, canvas2.width, canvas2.height)
                     graphicsContext1.strokeRect(
-                        min(baseX, event.x),
-                        min(baseY, event.y),
-                        abs(event.x - baseX),
-                        abs(event.y - baseY))
+                        round(min(baseX, event.x)),
+                        round(min(baseY, event.y)),
+                        round(abs(event.x - baseX)),
+                        round(abs(event.y - baseY)))
                 }
                 else -> {}
             }
@@ -144,13 +161,14 @@ class PaintApp : Application() {
             graphicsContext1.fill = colorPicker.value
             graphicsContext2.stroke = colorPicker.value
             graphicsContext2.fill = colorPicker.value
+            fillColor = colorPicker.value
         }
 
-        val sizeSlider = Slider(1.0, 100.0, 5.0)
-        val sizeLabel = Label("5")
-        sizeSlider.valueProperty().addListener { _: ObservableValue<out Number>, _: Number, newValue: Number ->
-            graphicsContext1.lineWidth = sizeSlider.value
-            graphicsContext2.lineWidth = graphicsContext1.lineWidth
+        val sizeSlider = Slider(1.0, 100.0, 2.0)
+        val sizeLabel = Label("2")
+        sizeSlider.valueProperty().addListener { _: ObservableValue<out Number>, _: Number, _: Number ->
+            graphicsContext1.lineWidth = round(sizeSlider.value)
+            graphicsContext2.lineWidth = round(graphicsContext1.lineWidth)
             sizeLabel.text = graphicsContext1.lineWidth.toInt().toString();
         }
 
@@ -163,6 +181,43 @@ class PaintApp : Application() {
             colorPicker,
             sizeSlider,
             sizeLabel)
+    }
+
+    private fun floodFill(x: Int, y: Int) {
+        val points = Array(canvas1.width.toInt()) {Array(canvas1.height.toInt()){false}}
+        val pixelReader = canvas1.snapshot(null, null).pixelReader
+        val color = pixelReader.getColor(x, y)
+        val pointsQueue = LinkedList<Pair<Int, Int>>()
+        pointsQueue.add(x to y)
+
+        while(!pointsQueue.isEmpty()) {
+            val (curX, curY) = pointsQueue.pollLast()
+            if (curX < 0 || curX >= points.size || curY < 0 || curY >= points[0].size) {
+                continue
+            }
+            if (
+                pixelReader.getColor(curX, curY) == color
+                && !points[curX][curY]) {
+                points[curX][curY] = true
+                pointsQueue.add(curX to (curY - 1))
+                pointsQueue.add(curX to (curY + 1))
+                pointsQueue.add((curX - 1) to curY)
+                pointsQueue.add((curX + 1) to curY)
+            }
+        }
+
+        val writer = graphicsContext1.pixelWriter
+        for (i in points.indices) {
+            for (j in points[0].indices) {
+                if (points[i][j]) {
+                    writer.setColor(i, j, fillColor)
+                }
+            }
+        }
+    }
+
+    private fun round(x: Double) : Double {
+        return x.toInt().toDouble()
     }
 }
 
